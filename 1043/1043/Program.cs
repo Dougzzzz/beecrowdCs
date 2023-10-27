ea858760ecf8917660ec2e4bef58f99ee0de0fe4
unknown <douglasbarcelos02@gmail.com>
2023-10-27 13:48:57 -0300

    exercicios 1050 e 1043 adicionados

---
+++ 1043/1043/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.ComponentModel.Design;
+
+string[] values = Console.ReadLine().Split();
+double a = Convert.ToDouble(values[0]);
+double b = Convert.ToDouble(values[1]);
+double c = Convert.ToDouble(values[2]);
+
+if ((a < b + c) && (b < a + c) && (c < a + b))
+{
+    Console.WriteLine($"Perimetro = {(a + b + c).ToString("F1")}");
+}
+else
+{
+    Console.WriteLine($"Area = {(((a + b) * c) / 2).ToString("F1")}");
+}
+
+Console.ReadLine();
